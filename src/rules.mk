2bfefbc5b07ef28a806e2722643082dcaba414af
Björn Bidar <theodorstormgrade@gmail.com>
2013-05-10 10:31:16 +0200

    updated build system

--- src/rules.mk
+++ src/rules.mk
@@ -1,9 +1,10 @@
 SHPP=shpp
 DESTDIR       =
 PREFIX        = /usr/local 
-INSTALL       = /usr/bin/env install 
+INSTALL       = /usr/bin/install -D
 MSGFMT        = /usr/bin/msgfmt
 SED           = /bin/sed
 bindir        = $(PREFIX)/bin
 libdir        = $(PREFIX)/lib
 sysconfdir    = $(PREFIX)/etc
+

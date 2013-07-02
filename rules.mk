5f7d818b6b016d0c5b6d6b294715eec1d6d6e061
Björn Bidar <theodorstormgrade@gmail.com>
2013-07-02 18:50:36 +0200

    updated

--- rules.mk
+++ rules.mk
@@ -1,10 +1,12 @@
 SHPP	      = shpp
 DESTDIR       =
-PREFIX        = /usr/local 
+PREFIX        = /usr/local
 INSTALL       = /usr/bin/install -D
 MSGFMT        = /usr/bin/msgfmt
 SED           = /bin/sed
 bindir        = $(PREFIX)/bin
 libdir        = $(PREFIX)/lib
 sysconfdir    = $(PREFIX)/etc
-
+datarootdir   = ${PREFIX}/share
+datadir       = ${datarootdir}
+mandir        = ${datarootdir}/man

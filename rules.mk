79f279ce56f076e06097f52b7d0d9ed0ad7b6cd9
Björn Bidar <theodorstormgrade@gmail.com>
2013-08-18 18:30:18 +0200

    added option to use included shpp (WBUILTIN_SHPP=1)

--- rules.mk
+++ rules.mk
@@ -1,4 +1,6 @@
-SHPP	      = shpp
+DIST_ROOT     = $(dir $(lastword $(MAKEFILE_LIST)))
+TOOLSDIR      = ${DIST_ROOT}tools
+
 DESTDIR       =
 PREFIX        = /usr/local
 INSTALL       = /usr/bin/install -D
@@ -10,3 +12,11 @@ sysconfdir    = $(PREFIX)/etc
 datarootdir   = ${PREFIX}/share
 datadir       = ${datarootdir}
 mandir        = ${datarootdir}/man
+
+SHPP	      = shpp
+BUILTIN_SHPP  = ${TOOLSDIR}/shpp
+WBUILTIN_SHPP = 0
+
+ifneq ($(WBUILTIN_SHPP),0)
+SHPP          = $(BUILTIN_SHPP)
+endif

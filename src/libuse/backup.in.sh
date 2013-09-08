38e7a5da22f81d5a34705c260b81ed44a6c04c3d
Björn Bidar <theodorstormgrade@gmail.com>
2013-09-09 00:24:58 +0200

    removed bashisms done some cleanup, added LIBUSE_VER

--- src/libuse/backup.in.sh
+++ src/libuse/backup.in.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # backup functions of libuse
 #
 # Copyright (C) 2013  Björn Bidar
@@ -23,16 +23,16 @@ if [ -z "$libsh_ver" ] ; then
   . ${libdir:-@prefix@/lib}/libsh || exit
 fi
  
-
- 
-
+LIBUSE_BACKUP_VER=1.1
+LIBUSE_VER=@LIBUSE_VER@
 
 ### functions for collect and restore data
 
 backup_data () { # backup application data
 #########################################
-# user_data     = path of user files for the  application
-# backup_option = sub-directory that is backupt by backup_data
+# user_data         = path of user files for the  application
+# backup_option     = sub-directory that is backupt by backup_data
+# backup_compressor = set compressor to tar archive with backup (default @LIBUSE_BACKUP_DCOMPRESSOR@)
 #########################################
   dt=$(date +%d_%m_%Y_%H_%M)
   mkdir -p "$user_data/backups"
@@ -42,8 +42,11 @@ backup_data () { # backup application data
   cat > "$user_data/backups/$dt"/source.info <<END
 BACKUPED_APPNAME="$APPNAME"
 copy_from='$user_data' 
-backup_time=( `date +%d_%m` `date +%Y_%H_%M` )
-compressor=xz
+backup_time_date=$(date +%Y_%H_%M)
+backup_time=$(date +%d_%)
+compressor=${backup_compressor:-xz}
+BACKUP_APPVER=$LIBUSE_BACKUP_VER
+BACKUP_APPNAME=libuse_back
 END
   tar -caf "$appname.$dt.tar.xz" "$user_data/$backup_option"
 }
@@ -54,10 +57,12 @@ restore_backup () { # restore backup archive that is made by backup_data
     tar --directory=$temp -xaf "$1" "$(tar -taf "$1" | grep source)"  && \ 
     . $temp/$(ls $temp)/source.info # search for source.info in  "$1", if no source.info found ; then display error 
     if [ ! $? = 0 ]; then
-      d_msg ! 'corupt file' "input file has no source.info (no restore backup archive or file broken?)" 
+      d_msg ! 'Corupt file' "input file has no source.info (no restore backup archive or file broken?)" 
       rm -rf $temp
       return 1
     fi
+    if [ -z $BACKUP_APPVER ] ; then
+	d_msg ! 'Incompatible File' 'File is incompatible with this version of libuse/backup($LIBUSE_VER)'
     # ask before overite target with backup if answer is no return with 1 ( d_msg returns 1 if answer is no)
     d_msg f overrwrite  "Realy overrwrite ${BACKUPED_APPNAME=`basename "$1"`} data from `echo ${backup_time[0]} at ${backup_time[1]:=(not set)} | sed 's/_/./g'` with $copy_from?" &&  \
 	tar --directory="$copy_from" -axf "$1"

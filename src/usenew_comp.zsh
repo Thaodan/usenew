6122779684f44c6262352307aebf80032bfeb31e
Björn Bidar <theodorstormgrade@gmail.com>
2013-10-29 16:21:54 +0100

    added zsh completition

---
+++ src/usenew_comp.zsh
@@ -0,0 +1,15 @@
+#compdef usenew -P '*.(exe|EXE|bat|cmd|reg|dll)'
+_arguments -s \
+    --debug'[help debugging]' \
+    {-h,--help}'[show help message]' \
+    -V'[show version]' \
+    {-g,--gui}'[enable gui output]' \
+    {-p,--prefix}'[ask for prefix]' \
+    {-d,--desktop}'[start file/command in virtual desktop]'
+
+# Local Variables:
+# mode: Shell-Script
+# sh-indentation: 2
+# indent-tabs-mode: nil
+# sh-basic-offset: 2
+# End:

e965a6559f4cf9171246440e4e5efe9c9ebb8c9f
Björn Bidar <theodorstormgrade@gmail.com>
2018-08-09 19:03:45 +0200

    libuse:tests:backup: check if backup went fine and check result

--- src/libuse/tests/backup_test.in.sh
+++ src/libuse/tests/backup_test.in.sh
@@ -1,12 +1,35 @@
 #!/bin/sh
-user_data=$PWD/testapp
+test_root=$PWD/testapp
+user_data=$test_root
 backup_option=test_data
 
 
 . "$PWD"/test_env_env
 . @libdir@/libuse/backup
 
+mkdir -p "$user_data/$backup_option"
+touch  "$user_data/$backup_option"/foo
+touch  "$user_data/$backup_option"/bar
 
 
+if ! backup_data ; then
+    d_msg ! 'test failed' 'Test failed early'
+    exit 1
+fi
 
-backup_data
+found=t
+
+backup_file="$(echo "$user_data"/backups/*)"
+
+for item in $(tar -taf "$backup_file" ) ; do
+    if [ ! -e "$user_data/$backup_option/$item" ] ; then
+        found=$item:$found
+    fi
+done 
+
+if [ ! "$found" = t ] ; then
+    d_msg ! 'test failed: archive' "following items are not found: $found"
+    exit 1 
+fi
+
+cleanup

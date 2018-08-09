8823d952a4c9a538efe7e275355d9f8ded904a39
Björn Bidar <theodorstormgrade@gmail.com>
2018-08-09 19:41:14 +0200

    libuse:tests: cleanup test_root first

--- src/libuse/tests/test_env_env.in.sh
+++ src/libuse/tests/test_env_env.in.sh
@@ -1,13 +1,13 @@
 # -*- sh -*-
 libdir=@libsh_libdir@
 
-
 cleanup()
 {
     if [ ! "$INSPECT" ] ; then
         rm -rf "${test_root:?}"
     fi
 }
+cleanup
 
 for signal in TERM HUP QUIT ERR; do
     trap "IID=1 cleanup; exit 1" $signal

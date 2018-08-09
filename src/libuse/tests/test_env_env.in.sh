2a030bc41de5ef8f3a9e68e8e75ca3a72b5d0562
Björn Bidar <theodorstormgrade@gmail.com>
2018-08-09 19:01:17 +0200

    libuse:testes:test_env_env: added basic test_env

---
+++ src/libuse/tests/test_env_env.in.sh
@@ -0,0 +1,16 @@
+# -*- sh -*-
+libdir=@libsh_libdir@
+
+
+cleanup()
+{
+    if [ ! "$INSPECT" ] ; then
+        rm -rf "${test_root:?}"
+    fi
+}
+
+for signal in TERM HUP QUIT ERR; do
+    trap "IID=1 cleanup; exit 1" $signal
+done
+unset signal
+trap "IID=1 cleanup; exit 130" INT
